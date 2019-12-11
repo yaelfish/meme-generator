ef67079243248417de9a218163999985ada41725
yaelfish <winkwinksituation@gmail.com>
2019-12-11 16:22:58 +0200

    render canvas pic from data

--- js/main.js
+++ js/main.js
@@ -3,14 +3,33 @@
 let gCanvas;
 let gCtx, gImg;
 let gCurrImg;
+let gElIng;
+let gLastX = 0;
+let gLastY = 0;
+
 
 function init(){
-
+    loadImages();
+    renderImages();
 }
 
-function onChosenImg(elImg) {  
+function renderImages() {
+    const elGallery = document.querySelector('.gallery-container');
+    let strHtml = ``;
+    let imgs = gImgs.map((img)=> {
+        strHtml+= `
+        <div class="img-container">
+            <img src="./images/gallery/${img.url}" data-img="${img.id}" onclick="onChosenImg(this,${img.id})">
+        </div>\n
+        `
+    });
+    elGallery.innerHTML = strHtml;
+}
+
+function onChosenImg(elImg,imgId) {  
     document.body.classList.add('canvas-open');
-    gCurrImg = elImg;
+    gCurrImg = findImgById(imgId);
+    gElIng = elImg;
     initCanvas();
 }
 
@@ -19,16 +38,59 @@ function initCanvas() {
     gCtx = gCanvas.getContext('2d');
     gCtx.fillStyle = 'black';
     drawImg();
+    drawText('This is the top', gCanvas.width / 2, 20);
+    drawText('This is the bottom', gCanvas.width / 2, gCanvas.height - 20);
 }
 
 function drawImg() {
-    if (gCurrImg)
-        gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height)
-    else {
-        gCurrImg = new Image()
-        gCurrImg.onload = () => {
-            gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height)
-        };
-        gCurrImg.src = './images/patrick.jpg'
+    if (gElIng) {
+        let src = `images/gallery/${gCurrImg.url}`;
+       
+        gCtx.drawImage(gElIng, 0, 0, gCanvas.width, gCanvas.height)
     }
+    else {
+        gElIng = new Image()
+        gElIng.onload = () => {
+            gCtx.drawImage(gElIng, 0, 0, gCanvas.width, gCanvas.height)
+        };
+        gElIng.src = './images/gallery/patrick.jpg'
+    }
+}
+
+function drawText(text, x, y) {
+    // const offsetX = event.offsetX;
+    // const offsetY = event.offsetY;
+    
+    gCtx.save()
+    gCtx.font = `${gCurrCanvas.fontSize}px ${gCurrCanvas.font}`;
+    gCtx.strokeStyle = gCurrCanvas.color;
+    gCtx.fillStyle = gCurrCanvas.bgColor;
+    gCtx.textAlign = 'center';
+    gCtx.textBaseline = 'middle';
+    gCtx.lineWidth = gCurrCanvas.lineWidth;
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+    gCtx.restore()
+}
+
+function draw(ev) {
+    gCtx.save()
+    const offsetX = ev.offsetX
+    const offsetY = ev.offsetY
+
+    switch (gCurrCanvas.shape) {
+        case 'triangle':
+            drawTriangle(offsetX, offsetY)
+            break;
+        case 'rectangle':
+            drawRect(offsetX, offsetY)
+            break;
+        case 'text':
+            drawText(gCurrCanvas.text, offsetX, offsetY)
+            break;
+        case 'line':
+            drawLine(offsetX, offsetY)
+            break;
+    }
+    gCtx.restore()
 }
--- js/memeService.js
+++ js/memeService.js
@@ -1,7 +1,66 @@
 'use strict';
 
-let gImgs = [];
+let gCurrCanvas = {};
+let gMemes = [];
 let gImgId = 0;
+
+let gImgs = [
+    {id: 1, url: '003.jpg', keywords: ['happy']},
+    {id: 2, url: '004.jpg', keywords: ['wow'] }
+];
+
+let gMeme = {
+    selectedImgId: 4,
+    selectedTxtIdx: 0,
+    txts: [
+        {
+            line: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+};
+
+
+
+function loadImages() {
+    return gImgs;
+}
+
+function findTextToRender(imgId) {
+    let textToRender;
+    gMemes.forEach((meme)=> {
+        if(meme.selectedImgId === imgId) textToRender = meme.txts[0]
+    });
+    return textToRender;
+}
+
+function findImgById(imgId) {
+    let imgToReturn = {};
+    gImgs.forEach((img) => {
+        if(img.id === imgId) {
+            imgToReturn = img;
+        }
+    });
+    return imgToReturn;
+}
+
+function findImgByIdx(params) {
+
+}
+
+function setCanvasPrefs() {
+    return gCurrCanvas = {
+        color: '#000000',
+        bgColor: '#ffffff',
+        shape: 'text',
+        lineWidth: '15',
+        text: 'Hello World',
+        font: 'meme-impact',
+        fontSize: 16
+    }
+}
 
 function createImages() {
     gImages.unshift(createImage('5.jpg', ['happy','in awe']));
@@ -10,9 +69,70 @@ function createImages() {
 }
 
 function createImage(url, keywords) {
-    return {
+    let img = {
         id: gImgId++,
         url: url,
         keywords: keywords
     }
+    return img;
+}
+
+function createMeme(selectedImgId, selectedTxtIdx, txts){
+    let gMeme = {
+        selectedImgId, 
+        selectedTxtIdx, 
+        txts
+    }
+    return gMeme;
+}
+
+function createMemes() {
+    gMemes.unshift(createMeme(1, 0, 'I never eat Falafel'));
+}
+
+
+function loadMemes() {
+    createMemes();
+}
+
+function createTxt(line, size, align, color) {
+    let gTxt = {
+        line,
+        size,
+        align,
+        color
+    }
+    return gTxt;
+}
+
+function createTxts() {
+    gTexts.unshift('I never eat Falafel', 20, 'left', '#FFBBFF')
+}
+
+function setShape(shape) {
+    gCurrCanvas.shape = shape;
+}
+
+function setColor(color) {
+    gCurrCanvas.color = color;
+}
+
+function setBgColor(bgColor) {
+    gCurrCanvas.bgColor = bgColor;
+}
+
+function setLineWidth(lineWidth) {
+    gCurrCanvas.lineWidth = lineWidth;
+}
+
+function setText(text) {
+    gCurrCanvas.text = text;
+}
+
+function setFontSize(fontSize) {
+    gCurrCanvas.fontSize = fontSize;
+}
+
+function getCurrCanvas() {
+    return gCurrCanvas;
 }
